c372455dd13a6b29ad52f6bed189185a047024e2
Orlando Joel Yagual Endara <DANIELCASTELAO\oyagualendara@nautilus31.ad.danielcastelao.org>
2019-11-19 09:25:05 +0100

    first commit

---
+++ boletin9_3/Boletin9_3.java
@@ -0,0 +1,12 @@
+
+package boletin9_3;
+
+
+public class Boletin9_3 {
+public static void main(String[] args) {
+  Rectangulo obx = new Rectangulo();
+      obx.CalcularArea();
+    }
+    
+}
+    
---
+++ boletin9_3/Rectangulo.java
@@ -0,0 +1,35 @@
+
+package boletin9_3;
+
+import java.util.Scanner;
+
+
+public class Rectangulo {
+
+
+    private double base;
+    private double area;
+    private double altura;
+    Scanner sc = new Scanner(System.in);
+    
+    public double pedirBase(){
+    System.out.print("Teclea la base: ");
+    base = sc.nextDouble();
+    return base;
+    }
+    public double pedirAltura(){
+    System.out.print("Teclea la altura: ");
+    altura = sc.nextDouble();
+    return altura;
+    }
+    public void CalcularArea(){
+    do{
+        base=pedirBase();
+        altura=pedirAltura();
+        area = base * altura;
+        }while(altura<0 || base<0);
+        System.out.println("A area de un rectángulo é "+area);
+    }
+    
+}
+
